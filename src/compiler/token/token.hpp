6f509c14c370bd5cde55bae30874ebe650a5ae88
PerHac13 <abdullah13112004@gmail.com>
2025-05-27 23:42:26 +0530

    feat: create token header file with necessary includes

---
+++ src/compiler/token/token.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "index.hpp"
+#include "delimiter/delimiter_token.hpp"
+#include "eof/eof_token.hpp"
+#include "identifier/identifier_token.hpp"
+#include "integer/integer_token.hpp"
+#include "keyword/keyword_token.hpp"
+#include "operator/operator_token.hpp"
