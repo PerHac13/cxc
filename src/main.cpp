7993dcf9cf8091f546575b3d4178c45a5d8fcb0c
PerHac13 <abdullah13112004@gmail.com>
2025-05-27 13:52:37 +0530

    chore!: main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <stdlib.h>
+
+#include "timer/timer.hpp"
+
+std::string get_filename(int argc, char *argv[]);
+
+int main(int argc, char *argv[])
+{
+
+    Timer timer = Timer();
+    timer.start("Compiler program started");
+
+    std::string filename = get_filename(argc, argv);
+
+    // Compiler entry point
+
+    // Compiler end point
+
+    timer.stop("Compiler program finished");
+
+    std::cout << "Total elapsed time: " << timer.time_val() << " seconds." << std::endl;
+    return 0;
+}
+
+std::string get_filename(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        std::cerr << "Error: No input file provided. \n"
+                  << std::endl;
+        std::cerr << "Usage: " << argv[0] << " <input_file>" << std::endl;
+        std::cerr << "Please provide a valid input file." << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    if (argc != 2)
+    {
+        std::cerr << "Error: Too many arguments provided. \n"
+                  << std::endl;
+        std::cerr << "Usage: " << argv[0] << " <input_file>" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    return std::string(argv[1]);
+}
